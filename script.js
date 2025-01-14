cdffc855df04af06b721d0c74cee297df4a09cc3
selogan <40747212+selogan@users.noreply.github.com>
2025-01-13 21:30:03 -0600

    feat: add etch-a-sketch functionality

---
+++ script.js
@@ -0,0 +1,49 @@
+const gridContainer = document.querySelector("#grid-container");
+gridContainer.addEventListener("mouseover", changeColor);
+
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const grid = document.createElement("div");
+        grid.setAttribute("id", "grid");
+        grid.style.height = gridContainer.offsetHeight / gridSize + "px";
+        grid.style.width = gridContainer.offsetWidth / gridSize + "px";
+        gridContainer.appendChild(grid);
+    }
+}
+
+const resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener("click", resetGrid);
+
+function resetGrid() {
+    const grids = document.querySelectorAll("#grid");
+    grids.forEach(grid => grid.style.backgroundColor = "white");
+}
+
+const gridSizeBtn = document.querySelector("#grid-size");
+gridSizeBtn.addEventListener("click", changeGridSize);
+
+function changeGridSize() {
+    let gridSize;
+    do {
+        gridSize = prompt("Please enter size of grid (1-100)");
+    } while (gridSize < 1 || gridSize > 100);
+
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+    createGrid(gridSize);
+}
+
+function changeColor(e) {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
+    let opacity = e.target.style.opacity;
+    if (Number(opacity) < 1) {
+        e.target.style.opacity = Number(opacity) + 0.1;
+    };
+}
+
+createGrid(16);
